aa5472e1f2e05c9c9f0914860ebdc2e056425516
Jerrie Pelser <jerrie@jerriepelser.com>
2018-11-14 19:16:34 +0700

    Login with oidc-client

---
+++ src/services/AuthService.ts
@@ -0,0 +1,41 @@
+import { UserManager, WebStorageStateStore, User } from "oidc-client";
+
+export default class AuthService {
+    private userManager: UserManager;
+
+    constructor() {
+        const AUTH0_DOMAIN: string = "https://jerrie.auth0.com";
+
+        const settings: any = {
+            userStore: new WebStorageStateStore({ store: window.localStorage }),
+            authority: AUTH0_DOMAIN,
+            client_id: "dnphgXcg0zeYBOCWwnRW8Xhq8Oif9A06",
+            redirect_uri: "http://localhost:8080/callback.html",
+            response_type: "id_token token",
+            scope: "openid profile",
+            post_logout_redirect_uri: "http://localhost:8080/",
+            filterProtocolClaims: true,
+            metadata: {
+                issuer: AUTH0_DOMAIN + "/",
+                authorization_endpoint: AUTH0_DOMAIN + "/authorize",
+                userinfo_endpoint: AUTH0_DOMAIN + "/userinfo",
+                end_session_endpoint: AUTH0_DOMAIN + "/v2/logout",
+                jwks_uri: AUTH0_DOMAIN + "/.well-known/jwks.json",
+            }
+        };
+
+        this.userManager = new UserManager(settings);
+    }
+
+    public getUser(): Promise<User> {
+        return this.userManager.getUser();
+    }
+
+    public login(): Promise<void> {
+        return this.userManager.signinRedirect();
+    }
+
+    public logout(): Promise<void> {
+        return this.userManager.signoutRedirect();
+    }
+}
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+    configureWebpack: {
+      plugins: [
+        new CopyWebpackPlugin([
+            { from: 'node_modules/oidc-client/dist/oidc-client.min.js', to: 'js' }
+        ])
+      ]
+    }
+  }
+  
